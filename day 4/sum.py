8648be8bdb5edcaa92ae8e9b0a84237f91021b35
Dipesh <titungdipesh1@gmail.com>
2026-06-11 16:08:41 +0545

    Initial commit

---
+++ day 4/sum.py
@@ -0,0 +1,8 @@
+num = 0
+total = 0
+
+input_num = int(input("Enter a number: "))
+while num <= input_num:
+    total=total+num
+    num=num+1
+print("The sum is: ", total)
---
+++ dipesh.py
@@ -0,0 +1,6 @@
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+      
